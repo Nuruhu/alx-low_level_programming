be013b7d22aebdd58d29e7355950d920fae21f31
Nuruhu <nuruhussen4040@gmail.com>
2022-09-09 08:41:10 -0700

    zero commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,13 +4,14 @@
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
+
+/* main - entry point */
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	
 	if (n > 0)
 		printf("%d is positive\n", n);
 	else if (n < 0)
