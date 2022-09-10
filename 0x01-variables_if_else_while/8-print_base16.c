e0a79a73755f63c7cfa71db5235ee4698be84d76
Nuruhu <nuruhussen4040@gmail.com>
2022-09-10 09:32:51 -0700

    8th commit

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - prints base 16 and a to f
+ *
+ * return: always zero it is nice
+ */
+int main(void)
+{
+	int x;
+	char y;
+
+	for (x = 0; x < 10; x++)
+		putchar((x % 10) + '0');
+	for (y = 'a'; y <= 'f'; y++)
+		putchar(y);
+	putchar('\n');
+	return (0);
+}
