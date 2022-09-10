ef1fe32ddc3c220298648e2a8ef01a92db1bf1f6
Nuruhu <nuruhussen4040@gmail.com>
2022-09-10 06:04:34 -0700

    2nd commit

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,11 +2,12 @@
 /**
  * main - prints all letters in lowercase alphabetical order
  *
- * return: always success
+ * return: always 0.
  */
 int main(void)
 {
 	char let;
+
 	for (let = 'a'; let<='z'; let++)
 		putchar(let);
 	putchar('\n');
