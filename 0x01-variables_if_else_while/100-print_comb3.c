65f8017edd4ea0d8de1b283ff651f6b124472e87
Nuruhu <nuruhussen4040@gmail.com>
2022-09-11 12:51:50 -0700

    100 commit

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - prints all possible combination of two numbers
+ *
+ * Return: Always zero (success)
+ */
+int main(void)
+{
+	int x,y;
+
+	for (x = 0; x < 9; x++)
+	{
+		for (y = x + 1; y < 10; y++)
+		{
+			putchar((x % 10) + '0');
+			putchar((y % 10) + '0');
+
+			if (x == 8 && y == 9)
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
