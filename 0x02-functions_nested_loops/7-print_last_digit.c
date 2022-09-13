74945a8754d607f5716b8d5a1ad6574cda1e728a
Nuruhu <nuruhussen4040@gmail.com>
2022-09-13 09:18:08 -0700

    7th commit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,9 +7,11 @@
  */
 int print_last_digit(int i)
 {
-	if (i < 0)
-		i = i * -1;
-	_putchar((i % 10) + '0');
-	return (i % 10);
+	int last_digit = i % 10;
+
+	if (last_digit < 0)
+		last_digit *= -1;
+	_putchar((last_digit + '0');
+	return (last_digit);
 }
 
