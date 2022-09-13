605dd7cd6e2ed6fb039f7fad35fe0f95237a0b95
Nuruhu <nuruhussen4040@gmail.com>
2022-09-13 10:39:38 -0700

    9th commit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,17 +8,26 @@ void times_table(void)
 
 	for (i = 0; i <= 9; i++)
 	{
-		_putchar('0');
-		for (j = 1; j <= 9; j++)
+		for (j = 0; j <= 9; j++)
+		{
+			k = (i * j);
+		if (j != 0)
 		{
 			_putchar(',');
 			_putchar(' ');
-			k = i * j;
-			if (k <= 9)
-				_putchar(' ');
-			else
-				_putchar((k / 10) + '0');
-			        _putchar((k % 10) + '0');
+		}
+		if (k >= 10)
+		{
+			_putchar((k / 10) + '0');
+			_putchar((k % 10) + '0');
+		}
+		else if (k < 10 && j != 0)
+		{
+			_putchar(' ');
+			_putchar((k % 10) + '0');
+		}
+		else
+			_putchar((k % 10) + '0');
 		}
 		_putchar('\n');
 	}
