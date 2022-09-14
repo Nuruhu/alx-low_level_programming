1f2c16e8f5973d6e600726e6d2a801d4a683dd5b
Nuruhu <nuruhussen4040@gmail.com>
2022-09-14 05:51:09 -0700

    102 commit

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,15 +7,21 @@
 int main(void)
 {
 	int a, b, i, sum;
+
 	a = 0;
 	b = 1;
-
 	for (i = 1; i <= 50; i++)
 	{
 		printf("%d", a);
 		sum = a + b;
 		a = b;
 		b = sum;
+		if (i == 50)
+			continue;
+		{
+	         putchar(',');
+                 putchar(' ');
+		}
 	}
 	printf("\n");
 	return (0);
