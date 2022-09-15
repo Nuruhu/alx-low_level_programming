0eeb9f1258b38c2ab21fc5bd8d23d4887342c0a9
Nuruhu <nuruhussen4040@gmail.com>
2022-09-15 09:07:24 -0700

    10th commit

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -6,26 +6,3 @@
  * Return: triangle
  */
 void print_triangle(int size)
-{
-	int x, y, z;
-
-	if (size <= 0)
-	{
-		_putchar('\n');
-	}
-	else
-	{
-		for (x = 0; x <= (size - 1); x++)
-		{
-			for (y = 0; y < (size - 1); y++)
-			{
-				_putchar(' ');
-			}
-			for (z = 0; z <= x; z++)
-			{
-				_putchar('#');
-			}
-			_putchar('\n');
-		}
-	}
-}
