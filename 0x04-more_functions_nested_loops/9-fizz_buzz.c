4bdc8ecc44271c6e48ec47b2e8f3f872c4b82985
Nuruhu <nuruhussen4040@gmail.com>
2022-09-15 08:24:02 -0700

    9th commit

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,23 +12,23 @@
 int main(void)
 {
 	int i;
-	char x[] = "Fizz";
-		char y[] = "Buzz";
-		char xy[] = "FizzBuzz";
+	char f[] = "Fizz";
+	char b[] = "Buzz";
+	char fb[] = "FizzBuzz";
 
-			for (i = 1; i <= 100; i++)
-			{
-				if (i == 100)
-					printf("%s", y);
-				else if ((i % 3 == 0) && (i % 5 == 0))
-					printf("%s", xy);
-				else if (i % 3 == 0)
-					printf("%s", x);
-				else if (i % 5 == 0)
-					printf("%s", y);
-				else 
-					printf("%d", i);
-			}
-		printf("\n");
-		return (0);
+	for (i = 1; i <= 100; i++)
+	{
+		if (i == 100)
+			printf("%s", b);
+		else if ((i % 3 == 0) && (i % 5 == 0))
+			printf("%s ", fb);
+		else if (i % 3 == 0)
+			printf("%s ", f);
+		else if (i % 5 == 0)
+			printf("%s ", b);
+		else
+			printf("%d ", i);
+	}
+	printf("\n");
+	return (0);
 }
