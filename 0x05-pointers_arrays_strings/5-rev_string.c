dbf8745b766b88e82db4ffd0650253cb30d3b4c7
Nuruhu <nuruhussen4040@gmail.com>
2022-09-19 06:13:34 -0700

    5th commit

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * rev_string - print reverse string
+ * @s: string input
+ */
+void rev_string(char *s)
+{
+	char rev = s[0];
+	int j = 0;
+	int i;
+
+	while (s[i] != '\0')
+		j++;
+	for (i = 0; i < j; i++)
+	{
+		j--;
+		rev = s[i];
+		s[i] = s[j];
+		s[j] = rev;
+	}
+}
