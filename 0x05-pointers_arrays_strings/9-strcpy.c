f9e5f385eeb24cc77edb5745d57b1f7251fbe34d
Nuruhu <nuruhussen4040@gmail.com>
2022-09-19 07:54:32 -0700

    9th commit

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strcpy - copy string
+ * @dest: copy to
+ * @src: copy from
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+}
