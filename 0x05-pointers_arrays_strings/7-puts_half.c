6f32d5e9552ee63e34fbd885b36cdf9cb4a48bf1
Nuruhu <nuruhussen4040@gmail.com>
2022-09-19 07:21:34 -0700

    7th commit

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,19 +1,19 @@
 #include "main.h"
 /**
- * puts_half - print half of string 
- * @str: string input
+ * put_half - prints half of the strong
+ * @str: str input
  */
 void puts_half(char *str)
 {
-	int i, n, len;
+	int i, j, half;
 
-	len = 0;
+	half = 0;
 	for (i = 0; str[i] != '\0'; i++)
-		len++;
-	n = (len / 2);
-	if ((len % 2) == 1)
-		n = ((len + 1) / 2);
-	for (i = n; str[i] != '\0'; i++)
+		half++;
+	j = (half / 2);
+	if ((half % 2) == 1)
+		j = ((half + 1) / 2);
+	for (i = j; str[i] != '\0'; i++)
 		_putchar(str[i]);
 	_putchar('\n');
 }
