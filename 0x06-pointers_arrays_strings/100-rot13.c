83fd76e518e4020e20680bb3f135be225abb03e6
Nuruhu <nuruhussen4040@gmail.com>
2022-09-21 14:00:50 -0700

    8th commit

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * rot13 - encodes a string using
+ * rotate by 13 places
+ * @n: pointer to string to be decoded
+ *
+ * Return: decoded value
+ */
+char *rot13(char *n)
+{
+	int i, j;
+	char l[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char dv[] ="NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; n[i] != '\0'; i++)
+	{
+		for (j = 0; l[j] != '\0'; j++)
+		{
+			if (n[i] == l[j])
+			{
+				n[i] = dv[j];
+				break;
+			}
+		}
+	}
+	return (n);
+}
