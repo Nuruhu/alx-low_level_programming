64a66c51a8aa94e0c4042402b677d8f642334897
Nuruhu <nuruhussen4040@gmail.com>
2022-09-21 09:38:23 -0700

    first commit

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -6,17 +6,4 @@
  *
  * Return: void
  */
-char *_strncat(char *dest, char *src, int n)
-{
-	int i, j;
 
-	for (i = 0; dest[i] != '\0'; i++)
-	{
-	}
-	for (j = 0; j < n; j++)
-	{
-		dest[i + j] = src[j];
-		if (src[j] == '\0')
-			j = n;
-	}
-}
