dfe51b7c7830a51158be1fd9f85bdab31474711e
Nuruhu <nuruhussen4040@gmail.com>
2022-09-21 13:18:53 -0700

    7th commit

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * leet - encodes a string
+ * @s: input
+ *
+ * Return: decoded value
+ */
+char *leet(char *s)
+{
+	int i, j;
+	int l[] = "aAeEoOtTlL";
+	int n[] = "43071";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			if (s[i] == l[j])
+			{
+				s[i] = n[j / 2];
+				j = 9;
+			}
+		}
+	}
+	return (s);
+}
