366fc7f777a4f8896e4a7ef34691960f91e6a24f
Nuruhu <nuruhussen4040@gmail.com>
2022-09-21 08:43:12 -0700

    zero commit

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strcat - function that concatenates two strings
+ * @dest: copy to
+ * @src: copy from
+ *
+ * Return: dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+	}
+	for (j = 0; (dest[i + j] = *src) != '\0'; j++)
+	{
+	}
+	return (dest);
+}
