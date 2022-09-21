1facb8ed553abc2d5b488119ec9522f60fec35aa
Nuruhu <nuruhussen4040@gmail.com>
2022-09-21 12:28:02 -0700

    the third comm

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,12 +8,13 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; ((s1[i] != '\0' && s2[i]) != '\0'); i++)
+	while (s1[i] != '\0' && s2[i] != '\0')
 	{
 		if (s1[i] != s2[i])
-			return (s1[i] - s2[i])
+			return (s1[i] - s2[i]);
+		i++;
 	}
 	if (s1[i] != '\0' || s2[i] != '\0')
 		return (s1[i] - s2[i]);
