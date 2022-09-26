861f406ac9e95cae0c9b0d694038b45f777dc77b
Nuruhu <nuruhussen4040@gmail.com>
2022-09-26 06:02:02 -0700

    4th commit

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _strpbrk -  searches a string for any of a set of bytes
+ * @s: source string
+ * @accept: accepted character
+ *
+ * Return: bytes
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i = 0;
+	int b;
+
+	while (s[i])
+	{
+		j = 0;
+		while (accept[i])
+		{
+			if (s[i] == accept[j])
+			{
+				s += i;
+				return(s);
+			}
+			j++;
+		}
+		i++;
+	}
+	return ('\0');
+}
