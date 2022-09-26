9ac50889c73d65eeccea73c905ba5c0168f63966
Nuruhu <nuruhussen4040@gmail.com>
2022-09-26 08:22:45 -0700

    100th commit

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * set_string - sets the value of pointer to char
+ * @s: adress of pointer
+ * @to: adress of value
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
