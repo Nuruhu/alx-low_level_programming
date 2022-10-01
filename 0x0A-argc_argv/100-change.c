b96acbe793074081891e0cfa88525cb5b9bb6204
Nuruhu <nuruhussen4040@gmail.com>
2022-09-30 17:34:35 -0700

    optional task

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -2,68 +2,62 @@
 #include <stdlib.h>
 #include "main.h"
 /**
- * coinConverter - Helper function that does all the mathematics
- * @i: Passed in variable from main for calculations
- * Return: The number of coins needed minimum for the passed in variable
+ * convCoin - convert user input argument to coin
+ * @c: argument to be converted
+ *
+ * Return: converted argument
  */
-int coinConverter(int i)
+int convCoin(int c)
 {
-	int count = 0;
+	int i = 0;
 
-	while (i != 0)
+	while (c != 0)
 	{
-		if (i % 10 == 9 || i % 10 == 7)
-			i -= 2;
-		else if (i % 25 == 0)
-			i -= 25;
-		else if (i % 10 == 0)
-			i -= 10;
-		else if (i % 5 == 0)
-			i -= 5;
-		else if (i % 2 == 0)
+		if (c % 10 == 9 || c % 10 == 7)
+			c = c - 2;
+		else if (c % 25 == 0)
+			c = c - 25;
+		else if (c % 10 == 0)
+			c = c - 10;
+		else if (c % 5 == 0)
+			c = c - 5;
+		else if (c % 2 == 0)
 		{
-			if (i % 10 == 6)
-				i -= 1;
+			if (c % 10 == 6)
+				c = c - 1;
 			else
-				i -= 2;
+				c = c - 2;
 		}
 		else
-			i -= 1;
-
-		count++;
+			c = c - 1;
+		i++;
 	}
-
-	return (count);
+	return (i);
 }
-
 /**
- * main - Takes in exactly one argument for minimum coin count
- * @argc: Number of command line arguments
- * @argv: Array name
- * Return: 0 if exactly 1 argument is passed into this program, 1 otherwise
+ * main - print the minimum number of coins
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: zero (success)
  */
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-	int i, coin;
-
-	coin = 0;
+	int x;
+	int coin = 0;
 
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-
-	i = atoi(argv[1]);
-
-	if (i < 0)
+	x = atoi(argv[1]);
+	if (x < 0)
 		printf("0\n");
 	else
 	{
-		coin = coinConverter(i);
-
+		coin = convCoin(x);
 		printf("%d\n", coin);
 	}
-
 	return (0);
 }
