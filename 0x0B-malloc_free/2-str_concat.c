8f26f146ae93e48b7f6b7a4f7449978a07e88860
Nuruhu <nuruhussen4040@gmail.com>
2022-10-03 08:34:46 -0700

    task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -24,7 +24,7 @@ char *str_concat(char *s1, char *s2)
 	ptr = Ss2;
 	while (*ptr++)
 		i++;
-	ptr = (char *)malloc(sizeof(char) * i);
+	ptr = (char *)malloc(sizeof(char) * i + 1);
 	if (ptr == NULL)
 		return (NULL);
 	i = 0;
